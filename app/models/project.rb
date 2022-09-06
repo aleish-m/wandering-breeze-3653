df07bd75912af4ab4609ce73437727a821a250b9
Aleisha Mork <aleisha.mork@gmail.com>
2022-09-05 22:52:10 -0600

    create active record many to many relationship between tables

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
+  has_many :contestants, through: :contestant_projects
+  has_many :contestant_projects
+
   validates_presence_of :name, :material
   belongs_to :challenge
 end
